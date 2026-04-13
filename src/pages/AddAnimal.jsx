2452de379ccbda84c334a4abb4470be60dc655c9
Alvi123787 <rebalalvi123@gmail.com>
2026-04-12 23:57:49 -0700

    update confirmation3  commit

--- src/pages/AddAnimal.jsx
+++ src/pages/AddAnimal.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useRef, useEffect } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
-import ReactQuill from 'react-quill'
-import 'react-quill/dist/quill.snow.css'
+import ReactQuill from 'react-quill-new'
+import 'react-quill-new/dist/quill.snow.css'
 import DOMPurify from 'dompurify'
 import {
   FaArrowLeft,
